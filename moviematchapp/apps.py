b8bde4169939a0dbc24371776691bbef21ee650a
josereddy7 <josereddy845@gmail.com>
2025-04-25 21:39:49 -0500

    movie

--- moviematchapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MoviematchappConfig(AppConfig):
-    default_auto_field = 'django_mongodb_backend.fields.ObjectIdAutoField'
-    name = 'moviematchapp'
--- usersapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersappConfig(AppConfig):
-    default_auto_field = 'django_mongodb_backend.fields.ObjectIdAutoField'
-    name = 'usersapp'
